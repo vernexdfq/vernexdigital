6874d4b64eb79fe8d3f086ddd88fef60691efe24
vernexdfq <official@vernex.com.ng>
2026-09-22 09:21:45 +0100

    Mount once-per-day CommunityPopup on home

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,12 @@
 import BottomNav from "@/components/BottomNav";
+import CommunityPopup from "@/components/CommunityPopup";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
       <BottomNav />
+      <CommunityPopup />
     </>
   );
 }
