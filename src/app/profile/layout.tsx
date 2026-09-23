2643931e62e2ca851c6570a2806c1bd0596e40a1
vernexdfq <official@vernex.com.ng>
2026-09-23 16:07:46 +0100

    feat: require login on profile

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,10 +1,11 @@
 import BottomNav from "@/components/BottomNav";
+import AuthGuard from "@/components/AuthGuard";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <AuthGuard>
       {children}
       <BottomNav />
-    </>
+    </AuthGuard>
   );
 }
