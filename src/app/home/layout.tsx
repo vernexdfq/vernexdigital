095872352ddd66c968e1717d7ab55521ee9b6649
vernexdfq <official@vernex.com.ng>
2026-09-23 16:07:35 +0100

    feat: require login on home (AuthGuard)

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,12 +1,13 @@
 import BottomNav from "@/components/BottomNav";
 import CommunityPopup from "@/components/CommunityPopup";
+import AuthGuard from "@/components/AuthGuard";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <AuthGuard>
       {children}
       <BottomNav />
       <CommunityPopup />
-    </>
+    </AuthGuard>
   );
 }
