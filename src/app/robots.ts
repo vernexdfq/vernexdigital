13aa831918a151a94c8e2f23e8b693335bff49f4
vernexdfq <official@vernex.com.ng>
2026-09-23 03:52:05 +0100

    fix: make robots sitemap use configured site URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://vernexdigital.com/sitemap.xml",
+    ...(process.env.NEXT_PUBLIC_SITE_URL ? { sitemap: process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "") + "/sitemap.xml" } : {}),
   };
 }
