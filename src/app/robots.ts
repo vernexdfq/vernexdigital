394caac3a89d76ad9e714556084c2090c5a5c250
vernexdfq <official@vernex.com.ng>
2026-09-23 03:55:27 +0100

    chore: point robots to Vernex production sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    ...(process.env.NEXT_PUBLIC_SITE_URL ? { sitemap: process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "") + "/sitemap.xml" } : {}),
+    sitemap: "https://vernexdigital.com/sitemap.xml",
   };
 }
